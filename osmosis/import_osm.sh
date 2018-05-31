4543148816e9cbc1fe40db6b5b4d97c622abfe9a
Rub21 <rub2106@gmail.com>
2018-05-31 14:49:18 -0500

    proces and upload pbf files

--- osmosis/import_osm.sh
+++ osmosis/import_osm.sh
@@ -2,6 +2,7 @@
 # Read the DB and create the planet osm file
 date=`date '+%Y-%m-%d:%H:%M'`
 planetFile=history-latest-${date}.osm
+planetPBFFile=history-latest-${date}.pbf
 planetFileCompress=$planetFile.bz2
 stateFile="state.txt"
 
@@ -13,10 +14,15 @@ password=$POSTGRES_PASSWORD \
 validateSchemaVersion=no \
 --write-xml \
 file=$planetFile
+
+# PBF File
+osmconvert $planetFile -o=$planetPBFFile
 # Compress the file
 bzip2 -v $planetFile
 # Save the path file
 echo "$S3_OSM_PATH/planet/full-history/$planetFileCompress" > $stateFile
+echo "$S3_OSM_PATH/planet/full-history/$planetPBFFile" >> $stateFile
 # Upload to S3
-aws s3 cp $planetFileCompress $S3_OSM_PATH/planet/full-history/$planetFileCompress --acl public-read
+aws s3 cp $planetPBFFile $S3_OSM_PATH/planet/full-history/$planetPBFFile
+aws s3 cp $planetFileCompress $S3_OSM_PATH/planet/full-history/$planetFileCompress
 aws s3 cp $stateFile $S3_OSM_PATH/planet/full-history/$stateFile --acl public-read
