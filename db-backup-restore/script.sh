91323f82d2eb0c50c47990f869b37c3e9b011acd
Rub21 <rub2106@gmail.com>
2018-06-21 12:16:49 -0500

    Storing data in S3 and GS

--- db-backup-restore/script.sh
+++ db-backup-restore/script.sh
@@ -1,34 +1,60 @@
 #!/usr/bin/env bash
 export PGPASSWORD=$POSTGRES_PASSWORD
-
-# Creating a gcloud-service-key to uthenticate the gcloud
-echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
-/root/google-cloud-sdk/bin/gcloud --quiet components update
-/root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
-/root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
-
 date=`date '+%Y-%m-%d:%H:%M'`
 backupFile=osm-seed-${date}.sql.gz
 stateFile="state.txt"
 restoreFile="backup.sql.gz"
 
+# Creating a gcloud-service-key to uthenticate the gcloud
+if [ $STORAGE == "GS" ]; then
+echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
+/root/google-cloud-sdk/bin/gcloud --quiet components update
+/root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
+/root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+fi
+
+# Backing up DataBase
 if [ "$DB_ACTION" = "backup" ]; then
     # Backup database and make maximum compression at the slowest speed
     /usr/bin/pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_DB  | gzip -9 > $backupFile
-    # Upload to S3
-    gsutil cp $backupFile $GS_OSM_PATH/database/$backupFile
-    # The file state.txt contain the later db path
-    echo "$GS_OSM_PATH/database/$backupFile" > $stateFile 
-    gsutil cp $stateFile $GS_OSM_PATH/database/$stateFile
-elif [ "$DB_ACTION" = "restore" ]; then
-    # Get the state.txt file from s3
-    gsutil cp $GS_OSM_PATH/database/$stateFile .
-    dbPath=$(head -n 1 $stateFile)
-    echo $dbPath
-    gsutil cp $dbPath $restoreFile
+
+    # AWS
+    if [ $STORAGE == "S3" ]; then 
+        # Upload to S3
+        aws s3 cp $backupFile $S3_OSM_PATH/database/$backupFile
+        # The file state.txt contain the latest version of DB path
+        echo "$S3_OSM_PATH/database/$backupFile" > $stateFile 
+        aws s3 cp $stateFile $S3_OSM_PATH/database/$stateFile
+    fi
+
+    # Google Storage
+    if [ $STORAGE == "GS" ]; then 
+        # Upload to GS
+        gsutil cp $backupFile $GS_OSM_PATH/database/$backupFile
+        # The file state.txt contain the latest version of DB path
+        echo "$GS_OSM_PATH/database/$backupFile" > $stateFile 
+        gsutil cp $stateFile $GS_OSM_PATH/database/$stateFile
+    fi
+fi
+
+# Restoring DataBase
+if [ "$DB_ACTION" = "restore" ]; then
+    # AWS
+    if [ $STORAGE == "S3" ]; then 
+        # Get the state.txt file from S3
+        aws s3 cp $S3_OSM_PATH/database/$stateFile .
+        dbPath=$(head -n 1 $stateFile)
+        aws s3 cp $dbPath $restoreFile
+    fi
+
+    # Google Storage
+    if [ $STORAGE == "GS" ]; then 
+    # Get the state.txt file from GS
+        gsutil cp $GS_OSM_PATH/database/$stateFile .
+        dbPath=$(head -n 1 $stateFile)
+        gsutil cp $dbPath $restoreFile
+    fi
+
     gzip -f -d $restoreFile
-    # Restore the database
     psql -h $POSTGRES_HOST -U $POSTGRES_USER  -d $POSTGRES_DB -f "${restoreFile%.*}"
-else
-    echo "The DB_ACTION = 'backup' or 'restore' must be set up"
 fi
--- osmosis/script.sh
+++ osmosis/script.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-
-# Creating a gcloud-service-key to uthenticate the gcloud
-echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
-/root/google-cloud-sdk/bin/gcloud --quiet components update
-/root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
-/root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
-
 # Read the DB and create the planet osm file
 date=`date '+%Y-%m-%d:%H:%M'`
 planetFile=history-latest-${date}.osm
@@ -13,6 +6,16 @@ planetPBFFile=history-latest-${date}.pbf
 planetFileCompress=$planetFile.bz2
 stateFile="state.txt"
 
+
+# Creating a gcloud-service-key to uthenticate the gcloud
+if [ $STORAGE == "GS" ]; then
+    echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
+    /root/google-cloud-sdk/bin/gcloud --quiet components update
+    /root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
+    /root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+fi
+
+# Creating the replication file
 osmosis --read-apidb \
 host=$POSTGRES_HOST \
 database=$POSTGRES_DB \
@@ -26,10 +29,25 @@ file=$planetFile
 osmconvert $planetFile -o=$planetPBFFile
 # Compress the file
 bzip2 -v $planetFile
-# Save the path file
-echo "$GS_OSM_PATH/planet/full-history/$planetFileCompress" > $stateFile
-echo "$GS_OSM_PATH/planet/full-history/$planetPBFFile" >> $stateFile
-# Upload to S3
-gsutil cp $planetPBFFile $GS_OSM_PATH/planet/full-history/$planetPBFFile
-gsutil cp $planetFileCompress $GS_OSM_PATH/planet/full-history/$planetFileCompress
-gsutil cp $stateFile $GS_OSM_PATH/planet/full-history/$stateFile
+
+# AWS
+if [ $STORAGE == "S3" ]; then 
+    # Save the path file
+    echo "$S3_OSM_PATH/planet/full-history/$planetFileCompress" > $stateFile
+    echo "$S3_OSM_PATH/planet/full-history/$planetPBFFile" >> $stateFile
+    # Upload to S3
+    aws s3 cp $planetPBFFile $S3_OSM_PATH/planet/full-history/$planetPBFFile
+    aws s3 cp $planetFileCompress $S3_OSM_PATH/planet/full-history/$planetFileCompress
+    aws s3 cp $stateFile $S3_OSM_PATH/planet/full-history/$stateFile --acl public-read
+fi
+
+# Google Storage
+if [ $STORAGE == "GS" ]; then 
+    # Save the path file
+    echo "$GS_OSM_PATH/planet/full-history/$planetFileCompress" > $stateFile
+    echo "$GS_OSM_PATH/planet/full-history/$planetPBFFile" >> $stateFile
+    # Upload to S3
+    gsutil cp $planetPBFFile $GS_OSM_PATH/planet/full-history/$planetPBFFile
+    gsutil cp $planetFileCompress $GS_OSM_PATH/planet/full-history/$planetFileCompress
+    gsutil cp $stateFile $GS_OSM_PATH/planet/full-history/$stateFile
+fi
