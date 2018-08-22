41513475c94d265d7596858102048e65e5405409
Rub21 <rub2106@gmail.com>
2018-08-22 11:07:09 -0500

    Add function to import the data

--- tiler-imposm/start.sh
+++ tiler-imposm/start.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 set -e
-# Import natural
-
-./scripts/natural_earth.sh
-./scripts/osm_land.sh
+mkdir -p /tmp
 
 FILE='http://download.geofabrik.de/south-america/peru-latest.osm.pbf'
 PBF='peru-latest.osm.pbf'
-wget -O $PBF $FILE
-imposm import -connection postgis://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB -mapping imposm3.json -read $PBF -write
-imposm import -connection postgis://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB -mapping imposm3.json -deployproduction
+flag=true
+function importData () {
+    echo "Execute the missing functions"
+    psql "postgresql://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB" -a -f postgis_helpers.sql
+    psql "postgresql://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB" -a -f postgis_index.sql
+    echo "Import Natural Earth"
+    ./scripts/natural_earth.sh
+    echo "Impor OMS Land"
+    ./scripts/osm_land.sh
+    echo "Import PBF file"
+    wget -O $PBF $FILE
+    imposm import -connection postgis://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB -mapping imposm3.json -read $PBF -write
+    imposm import -connection postgis://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB -mapping imposm3.json -deployproduction
+}
 
-# Run the missing functions
+while "$flag" = true; do
+    pg_isready -h $GIS_POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
+        # Change flag to false to stop ping the DB
+        flag=false
+        hasData=$(psql "postgresql://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB" \
+        -c "SELECT count(*) FROM information_schema.tables WHERE table_schema = 'public'" | sed -n 3p | sed 's/ //g')
+        # After import there are more than 70 tables
+        if [ $hasData  \> 70 ]; then
+            echo "Update the DB with osm data"
+        else
+            echo "Start importing the data"
+            importData
+        fi
+done
 
-psql "postgresql://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB" -a -f postgis_helpers.sql
-psql "postgresql://$GIS_POSTGRES_USER:$GIS_POSTGRES_PASSWORD@$GIS_POSTGRES_HOST/$GIS_POSTGRES_DB" -a -f postgis_index.sql
-mkdir -p /tmp
