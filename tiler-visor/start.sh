62236f4603854b620214268e263b888e071962be
Rub21 <rub2106@gmail.com>
2018-08-23 16:49:38 -0500

    Add env TILER_VISOR_PROTOCOL  for visor

--- tiler-visor/start.sh
+++ tiler-visor/start.sh
@@ -2,5 +2,6 @@
 set -e
 sed -i -e 's/demo.tegola.io/'$TILER_SERVER_HOST:$TILER_SERVER_PORT'/g' /usr/share/nginx/html/config.json
 sed -i -e 's/localhost:9090/'$TILER_SERVER_HOST:$TILER_SERVER_PORT'/g' /usr/share/nginx/html/capabilities/osm.json
-sed -i -e 's,https://osm.tegola.io,'${http://$TILER_VISOR_HOST:$TILER_VISOR_PORT}',g' /usr/share/nginx/html/config.json
+tiler_visor_url=$TILER_VISOR_PROTOCOL://$TILER_VISOR_HOST:$TILER_VISOR_PORT
+sed -i -e 's,https://osm.tegola.io,'${tiler_visor_url}',g' /usr/share/nginx/html/config.json
 nginx -g 'daemon off;'
