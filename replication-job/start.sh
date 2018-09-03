06c3f9bdc14bd02f1684b1b41f4ff3fef08d3063
Rub21 <rub2106@gmail.com>
2018-09-03 17:11:23 -0500

    Fix the functions in the DB

--- replication-job/start.sh
+++ replication-job/start.sh
@@ -22,22 +22,3 @@ password=$POSTGRES_PASSWORD \
 allowIncorrectSchemaVersion=true \
 --write-replication \
 workingDirectory=$workingDirectory
-
-# osmosis -q --send-replication-data dataDirectory=data port=8080 notificationPort=8081
-
-# # AWS
-# if [ $STORAGE == "S3" ]; then 
-#     # Save the path file
-#     echo "$S3_OSM_PATH/planet/replicate-minute/$planetFileCompress" > $stateFile
-
-#     # Upload to S3
-#     aws s3 cp $planetPBFFile $S3_OSM_PATH/planet/full-history/$planetPBFFile
-#     aws s3 cp $planetFileCompress $S3_OSM_PATH/planet/full-history/$planetFileCompress
-#     aws s3 cp $stateFile $S3_OSM_PATH/planet/full-history/$stateFile --acl public-read
-# fi
-
-# # Google Storage
-# if [ $STORAGE == "GS" ]; then 
-#     # Save the path file
-#     gsutil cp -r $workingDirectory $GS_OSM_PATH/minute
-# fi
