148a7ef6ff82edb80a89960ea0a59b2472cf2bbc
Rub21 <rub2106@gmail.com>
2018-09-04 12:16:33 -0500

    Storing the files in buckets

--- replication-job/README.md
+++ replication-job/README.md
@@ -1,11 +1,52 @@
 ### Osmosis Replication job container
 
+This contain is responsible for creating the replication files, and then upload to a repository in AWs of Gooble Storage, depends on what you are using.
+
+This container must be configured through a cron job to be executed.
+
 ### Configuration
+
+In order to run this container, we should pass some environment variables
+
+**Postgres envs**
+
+- `POSTGRES_HOST` e.g db
+- `POSTGRES_DB` e.g openstreetmap
+- `POSTGRES_USER` e.g postgres
+- `POSTGRES_PASSWORD`  e.g 1234
+
+**Replication interval, in minutes**
+
+- `REPLICATION_INTERVAL` e.g 3 minutes.
+
+**Storage envs**
+
+- `STORAGE`, eg. `S3` or `GS`
+
+In case AWS:
+
+- `AWS_ACCESS_KEY_ID`
+- `AWS_SECRET_ACCESS_KEY`
+- `AWS_DEFAULT_REGION`
+- `S3_OSM_PATH` e.g `s3://osm-seed-test`
+
+In case Google storage:
+
+- `GS_OSM_PATH` e.g `gs://osm-seed-test`
+- `GCLOUD_SERVICE_KEY`
+- `GCLOUD_PROJECT`
+
+#### Building the container
+
+```
+  docker build -t openseed-replication-job:v1 .
+```
 
 #### Running the container
 
 ```
-docker run --env-file ./../.env \
---network osm-seed_default \
--it openseed-replication-job:v1 bash
+  docker run --env-file ./../.env \
+  --network osm-seed_default \
+  -v $(pwd)/../replication-files:/app/data \
+  -t openseed-replication-job:v1
 ```
--- replication-job/start.sh
+++ replication-job/start.sh
@@ -2,18 +2,13 @@
 stateFile="state.txt"
 workingDirectory="data"
 minInterval=$(( $REPLICATION_INTERVAL * 60 * 1000 ))
-# Creating a gcloud-service-key to authenticate the gcloud
-if [ $STORAGE == "GS" ]; then
-    echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
-    /root/google-cloud-sdk/bin/gcloud --quiet components update
-    /root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
-    /root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
-fi
+maxInterval=$(( $REPLICATION_INTERVAL * 6 * 60 * 1000 ))
 
 # Creating the replication file
 osmosis -q \
 --replicate-apidb \
 minInterval=$minInterval \
+maxInterval=$maxInterval \
 iterations=1 \
 host=$POSTGRES_HOST \
 database=$POSTGRES_DB \
@@ -22,3 +17,21 @@ password=$POSTGRES_PASSWORD \
 allowIncorrectSchemaVersion=true \
 --write-replication \
 workingDirectory=$workingDirectory
+
+# AWS
+if [ $STORAGE == "S3" ]; then 
+    # Sync to S3
+    aws s3 sync $workingDirectory $S3_OSM_PATH/replication/changesets
+fi
+
+# Google Storage
+if [ $STORAGE == "GS" ]; then
+    # Creating a gcloud-service-key to authenticate the gcloud
+    echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
+    /root/google-cloud-sdk/bin/gcloud --quiet components update
+    /root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud-service-key.json
+    /root/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+
+    # Sync to GS
+    gsutil rsync -r $workingDirectory $GS_OSM_PATH/replication/changesets
+fi
