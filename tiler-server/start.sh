858288622cf536215775bf664aafeb20d6e6a73a
Rub21 <rub2106@gmail.com>
2020-05-21 17:25:38 -0500

    Fix dependencies and find files in last min ago

--- tiler-server/start.sh
+++ tiler-server/start.sh
@@ -2,12 +2,23 @@
 set -e
 flag=true
 
-imposm3_expire_dir=/mnt/data/imposm3_expire_dir
-mkdir -p $imposm3_expire_dir
+IMPOSM3_EXPIRE_DIR=/mnt/data/imposm3_expire_dir
+mkdir -p $IMPOSM3_EXPIRE_DIR
+
+IMPOSM3_EXPIRE_PURGED=/mnt/data/imposm3_expire_dir_purged
+mkdir -p $IMPOSM3_EXPIRE_PURGED
+
+function purgeCache(){
+  
+}
 
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
   flag=false
   # TEGOLA_SQL_DEBUG=EXECUTE_SQL 
-  tegola serve --config=/opt/tegola_config/config.toml
+  tegola serve --config=/opt/tegola_config/config.toml &
+	while true; do
+    purgeCache
+		sleep 30s
+	done
 done
