e6e6ce7dd225e72cd63115bb6709aad43cd3a42a
Rub21 <rub2106@gmail.com>
2020-05-22 16:53:17 -0500

    Fix downloader for tiles

--- tiler-server/start.sh
+++ tiler-server/start.sh
@@ -6,42 +6,48 @@ mkdir -p $imposm3_expire_dir
 imposm3_expire_purged=$workDir/imposm3_expire_dir_purged
 mkdir -p $imposm3_expire_purge
 
-function purgeCache(){
-  echo "Purge cache..."
-  # Get S3 tiles files from the last 1 minute 
-  # TODO, Set the time apgo in ENV vars
-  DATE=$(date -d '-1 min' +'%m/%d/%Y %H:%M:%S')
-  if [ "$CLOUDPROVIDER" == "aws" ]; then
-    aws s3api list-objects-v2 \
-    --bucket planet.openhistoricalmap.org \
-    --query "Contents[?LastModified>'$DATE']" > last_upload_files.json
-    # Filter tiles with extencion .tiles
-    cat last_upload_files.json | jq -c '[ .[] | select( .Key | contains(".tiles")) ]' | jq '.[].Key' > list.list
-    sed 's/\"//g' list.list > list_expired.list
-    # Download tiles
-    while IFS= read -r tileFile
-    do
-      aws s3 cp ${AWS_S3_BUCKET}/${tileFile} $workDir/${tileFile}
-      # Purge
-      echo tegola --config=/opt/tegola_config/config.toml cache purge --tile-list=$workDir/${tileFile} --tile-name-format="/zxy" --min-zoom 0 --max-zoom 20  --overwrite
-      # err=$?
-      # if [[ $err != "0" ]]; then
-      #   #error
-      #   echo "tegola exited with error code $err"
-      # fi
-    done < list_expired.list
-  fi
+function downloadExpiredTiles(){
+    echo "Download expired tiles...."
+    # Get S3 tiles files from the last 1 minute  
+    dateStr="$(date -d '-1 min' +'%Y-%m-%dT%H:%M:%S.000Z')"
+    # 2020-05-22T19:51:05.000Z
+    # PURGE_CACHE_FROM = -1 min, by default
+    # eg. -50 min, -4 hour, -4 day
+    # if [[ ! -z "$PURGE_TILE_CACHE_FROM" ]]
+    # then
+    #   dateStr=$(date -d $PURGE_TILE_CACHE_FROM +'%m/%d/%Y %H:%M:%S')
+    # fi
+    # echo $PURGE_TILE_CACHE_FROM
+    echo "Getting expired tiles from... $dateStr"
+    # TODO downloader for GS
+    if [ "$CLOUDPROVIDER" == "aws" ]; then
+
+        # Download the list of latets files according to dateStr
+        aws s3api list-objects-v2 \
+        --bucket ${AWS_S3_BUCKET#*"s3://"} \
+        --query "Contents[?LastModified>'$dateStr']" > $workDir/imposm/tile_expired.json
+
+        # Filter tiles with extencion .tiles
+        cat $workDir/imposm/tile_expired.json | \
+        jq -c '[ .[] | select( .Key | contains(".tiles")) ]' | \
+        jq '.[].Key' | \
+        sed 's/\"//g' > $workDir/imposm/tiles_expired.list
+
+        while IFS= read -r tileFile
+        do
+            aws s3 cp ${AWS_S3_BUCKET}/${tileFile} $workDir/${tileFile}
+        done < $workDir/imposm/tiles_expired.list
+    fi
 }
 
 flag=true
-
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
   flag=false
   # TEGOLA_SQL_DEBUG=EXECUTE_SQL 
   tegola serve --config=/opt/tegola_config/config.toml &
 	while true; do
-    purgeCache
-		sleep 30s
+    downloadExpiredTiles
+		sleep 1m
 	done
 done
