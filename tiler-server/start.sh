639ade93fdfea5e3bf9e97acd25ba458365e14ea
Rub21 <rub2106@gmail.com>
2020-05-21 19:56:22 -0500

    Purge tiles and disable no used containers

--- tiler-server/start.sh
+++ tiler-server/start.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -e
-flag=true
 
-imposm3_expire_dir=/mnt/data/imposm3_expire_dir
+workDir=/mnt/dat
+imposm3_expire_dir=$workDir/imposm/imposm3_expire_dir
 mkdir -p $imposm3_expire_dir
-
-imposm3_expire_purged=/mnt/data/imposm3_expire_dir_purged
-mkdir -p $imposm3_expire_purged
+imposm3_expire_purged=$workDir/imposm3_expire_dir_purged
+mkdir -p $imposm3_expire_purge
 
 function purgeCache(){
   echo "Purge cache..."
@@ -18,15 +16,24 @@ function purgeCache(){
     --bucket planet.openhistoricalmap.org \
     --query "Contents[?LastModified>'$DATE']" > last_upload_files.json
     # Filter tiles with extencion .tiles
-    cat last_upload_files.json | jq -c '[ .[] | select( .Key | contains(".tiles")) ]' | jq '.[].Key' > list_expired.list
+    cat last_upload_files.json | jq -c '[ .[] | select( .Key | contains(".tiles")) ]' | jq '.[].Key' > list.list
+    sed 's/\"//g' list.list > list_expired.list
     # Download tiles
     while IFS= read -r tileFile
     do
-      aws s3 cp ${AWS_S3_BUCKET}/${tileFile} $imposm3_expire_dir
+      aws s3 cp ${AWS_S3_BUCKET}/${tileFile} $workDir/${tileFile}
+      # Purge
+      tegola --config=/opt/tegola_config/config.toml cache purge --tile-list=$workDir/${tileFile} --tile-name-format="/zxy" --min-zoom 0 --max-zoom 20  --overwrite
+      err=$?
+      if [[ $err != "0" ]]; then
+        #error
+        echo "tegola exited with error code $err"
+      fi
     done < list_expired.list
-    # Purge
   fi
 }
+
+flag=true
 
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
