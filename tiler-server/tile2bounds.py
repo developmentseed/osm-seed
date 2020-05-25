1ba0084737080ec2cffe92006efdab5f64068413
Rub21 <rub2106@gmail.com>
2020-05-25 15:20:59 -0500

    Fix print for python version

--- tiler-server/tile2bounds.py
+++ tiler-server/tile2bounds.py
@@ -5,8 +5,8 @@ usage:
       python tile2bounds.py 14/4814/8796
 """
 import sys
-from mercantile import  bounds, parent , Tile
+from mercantile import bounds, parent, Tile
 
-arrayTile=str(sys.argv[1]).split('/')
+arrayTile = str(sys.argv[1]).split('/')
 lnglatbbox = bounds(Tile(x=int(arrayTile[1]), y=int(arrayTile[2]), z=int(arrayTile[0])))
-print(f'{lnglatbbox[0]},{lnglatbbox[1]},{lnglatbbox[2]},{lnglatbbox[3]}')
+print(str(lnglatbbox[0]) + "," + str(lnglatbbox[1]) + "," + str(lnglatbbox[2]) + "," + str(lnglatbbox[3]))
