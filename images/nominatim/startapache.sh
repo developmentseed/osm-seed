875caf98298ace9875f141f7a84042a6f68dc2ab
Rub21 <rub2106@gmail.com>
2021-02-17 12:59:40 -0500

    Set ARGS as ENV Vars adn running in docker-compose

--- images/nominatim/startapache.sh
+++ images/nominatim/startapache.sh
@@ -2,6 +2,10 @@
 mv /app/src/build/settings/local_db.php /app/src/build/settings/local.php
 export NOMINATIM_SETTINGS='/app/src/build/settings/local.php'
 
-/usr/sbin/apache2ctl -D FOREGROUND
-tail -f /var/log/apache2/error.log
-
+flag=true
+while "$flag" = true; do
+    pg_isready -h $PG_HOST -p $PG_PORT >/dev/null 2>&2 || continue
+    flag=false
+    /usr/sbin/apache2ctl -D FOREGROUND &&
+        tail -f /var/log/apache2/error.log
+done
