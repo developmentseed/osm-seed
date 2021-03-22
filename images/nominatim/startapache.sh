072722d398e1901a2ce446923b008a22946d9e33
Sajjad Anwar <sajjadkm@gmail.com>
2021-03-22 13:07:37 +0530

    remove  reserved php envs. add setup.php before apache start

--- images/nominatim/startapache.sh
+++ images/nominatim/startapache.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export NOMINATIM_SETTINGS='/app/src/build/settings/local_api.php'
+export NOMINATIM_SETTINGS='/app/src/build/settings/local.php'
+echo 'Setup the app'
+/app/src/build/utils/setup.php --setup-website
 
 flag=true
 while "$flag" = true; do
