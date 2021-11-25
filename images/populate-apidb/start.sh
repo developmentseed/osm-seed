6881e3e36b42bb5aae92eba0084b1a628155d497
Rub21 <ruben@developmentseed.org>
2021-11-25 13:49:13 -0500

    Fix import pbf fiel to db-api

--- images/populate-apidb/start.sh
+++ images/populate-apidb/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export VOLUME_DIR=/mnt/data
+export PGPASSWORD=$POSTGRES_PASSWORD
 
 # OSMOSIS tuning: https://wiki.openstreetmap.org/wiki/Osmosis/Tuning,https://lists.openstreetmap.org/pipermail/talk/2012-October/064771.html
 if [ -z "$MEMORY_JAVACMD_OPTIONS" ]; then
@@ -7,14 +9,18 @@ else
     memory="${MEMORY_JAVACMD_OPTIONS//i}"
     echo JAVACMD_OPTIONS=\"-server -Xmx$memory\" > ~/.osmosis
 fi
+
 # Get the data
-wget $URL_FILE_TO_IMPORT
 file=$(basename $URL_FILE_TO_IMPORT)
+osmFile=$VOLUME_DIR/$file
+[ ! -f $osmFile ] && wget $URL_FILE_TO_IMPORT
 
 function importData () {
+    # This is using a osmosis 0.47. TODO: test with osmosis 0.48, and remove the following line
+    psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "ALTER TABLE users ADD COLUMN nearby VARCHAR;"
     # In case the import file is a PBF
-    if [ ${file: -4} == ".pbf" ]; then
-        pbfFile=$file
+    if [ ${osmFile: -4} == ".pbf" ]; then
+        pbfFile=$osmFile
         echo "Importing $pbfFile ..."
         osmosis --read-pbf \
         file=$pbfFile\
@@ -23,12 +29,13 @@ function importData () {
         database=$POSTGRES_DB \
         user=$POSTGRES_USER \
         password=$POSTGRES_PASSWORD \
+        allowIncorrectSchemaVersion=yes \
         validateSchemaVersion=no
     else
         # In case the file is .osm
         # Extract the osm file
-        bzip2 -d $file
-        osmFile=${file%.*}
+        bzip2 -d $osmFile
+        osmFile=${osmFile%.*}
         echo "Importing $osmFile ..."
         osmosis --read-xml \
         file=$osmFile  \
@@ -39,6 +46,13 @@ function importData () {
         password=$POSTGRES_PASSWORD \
         validateSchemaVersion=no
     fi
+    # Run required fixes in DB
+    psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "select setval('current_nodes_id_seq', (select max(node_id) from nodes));"
+    psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "select setval('current_ways_id_seq', (select max(way_id) from ways));"
+    psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "select setval('current_relations_id_seq', (select max(relation_id) from relations));"
+    # psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "select setval('users_id_seq', (select max(id) from users));"
+    # psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "select setval('changesets_id_seq', (select max(id) from changesets));"
+
 }
 
 flag=true
