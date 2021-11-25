e9ee924eec7e9263fdcc1a0b4251e1d606f7f017
Rub21 <ruben@developmentseed.org>
2021-11-25 16:26:21 -0500

    Remove env files and update docker-compose

---
+++ envs/envs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cp ./envs/.env.web.example ./envs/.env.web
+cp ./envs/.env.db.example ./envs/.env.db
+cp ./envs/.env.cloudprovider.example ./envs/.env.cloudprovider
+cp ./envs/.env.planet-dump.example ./envs/.env.planet-dump
+cp ./envs/.env.backup-restore.example ./envs/.env.backup-restore
+cp ./envs/.env.replication-job.example ./envs/.env.replication-job
+cp ./envs/.env.full-history.example ./envs/.env.full-history
+cp ./envs/.env.populate-apidb.example ./envs/.env.populate-apidb
+cp ./envs/.env.tiler-db.example ./envs/.env.tiler-db
+cp ./envs/.env.tiler-imposm.example ./envs/.env.tiler-imposm
+cp ./envs/.env.tiler-server.example ./envs/.env.tiler-server
+cp ./envs/.env.nominatim.example ./envs/.env.nominatim
+cp ./envs/.env.overpass.example ./envs/.env.overpass
+cp ./envs/.env.tasking-manager.example ./envs/.env.tasking-manager
+cp ./envs/.env.taginfo.example ./envs/.env.taginfo
