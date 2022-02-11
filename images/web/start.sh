7ef2d9e65ea309ce3f2666836289cde617742e12
Rub21 <ruben@developmentseed.org>
2022-02-11 11:32:11 -0500

    Config Mail sender - web container

--- images/web/start.sh
+++ images/web/start.sh
@@ -3,7 +3,8 @@ workdir="/var/www"
 
 export RAILS_ENV=production
 # Because we can not set up many env variable sin build process, we are going to process here!
-# Setting up the production database
+
+#### SETTING UP THE PRODUCTION DATABASE
 echo " # Production DB
 production:
   adapter: postgresql
@@ -13,9 +14,18 @@ production:
   password: ${POSTGRES_PASSWORD}
   encoding: utf8" >$workdir/config/database.yml
 
-# Setting up the SERVER_URL and SERVER_PROTOCOL
+#### SETTING UP SERVER_URL AND SERVER_PROTOCOL
 sed -i -e 's/server_url: "openstreetmap.example.com"/server_url: "'$SERVER_URL'"/g' $workdir/config/settings.yml
 sed -i -e 's/server_protocol: "http"/server_protocol: "'$SERVER_PROTOCOL'"/g' $workdir/config/settings.yml
+
+#### SETTING UP MAIL SENDER
+sed -i -e 's/smtp_address: "localhost"/smtp_address: "'$MAILER_ADDRESS'"/g' $workdir/config/settings.yml
+sed -i -e 's/smtp_domain: "localhost"/smtp_domain: "'$MAILER_DOMAIN'"/g' $workdir/config/settings.yml
+sed -i -e 's/smtp_enable_starttls_auto: false/smtp_enable_starttls_auto: true/g' $workdir/config/settings.yml
+sed -i -e 's/smtp_authentication: null/smtp_authentication: "login"/g' $workdir/config/settings.yml
+sed -i -e 's/smtp_user_name: null/smtp_user_name: "'$MAILER_USERNAME'"/g' $workdir/config/settings.yml
+sed -i -e 's/smtp_password: null/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir/config/settings.yml
+sed -i -e 's/email_from: "OpenStreetMap <openstreetmap@example.com>"/email_from: "'$MAILER_FROM'"/g' $workdir/config/settings.yml
 
 # Check if DB is already up
 flag=true
@@ -35,6 +45,6 @@ while "$flag" = true; do
   # Start the delayed jobs queue worker
   # bundle exec rake jobs:work
   # Start the app
-  # bundle exec rails server
+  bundle exec rake jobs:work &
   apachectl -k start -DFOREGROUND
 done
