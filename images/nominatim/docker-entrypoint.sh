30379a63f5941ff657c36517539a8b8ac83a680f
Rub21 <ruben@developmentseed.org>
2022-09-27 09:34:35 -0500

    Set env var into env file - nominatim

--- images/nominatim/docker-entrypoint.sh
+++ images/nominatim/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
+CONFIG_FILE=${PROJECT_DIR}/.env
+
 if [[ ! -z "$NOMINATIM_ADDRESS_LEVEL_CONFIG_URL" ]]; then
     curl -o /app/address-levels.json "${NOMINATIM_ADDRESS_LEVEL_CONFIG_URL}"
-    export NOMINATIM_ADDRESS_LEVEL_CONFIG=/app/address-levels.json
+    echo NOMINATIM_ADDRESS_LEVEL_CONFIG=/app/address-levels.json >> ${CONFIG_FILE}
 fi
 /app/start.sh
