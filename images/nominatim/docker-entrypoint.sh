d02997cea750d6842d89dfe16b0dd550ffe1e783
Rub21 <ruben@developmentseed.org>
2022-09-29 09:54:45 -0500

    Update env vars

--- images/nominatim/docker-entrypoint.sh
+++ images/nominatim/docker-entrypoint.sh
@@ -5,4 +5,9 @@ if [[ ! -z "$NOMINATIM_ADDRESS_LEVEL_CONFIG_URL" ]]; then
     curl -o /app/address-levels.json "${NOMINATIM_ADDRESS_LEVEL_CONFIG_URL}"
     echo NOMINATIM_ADDRESS_LEVEL_CONFIG=/app/address-levels.json >> ${CONFIG_FILE}
 fi
+
+if [[ ! -z "$OSMSEED_WEB_API_DOMAIN" ]]; then
+    find /usr/local/lib/nominatim/lib-python/nominatim/ -type f | xargs perl -pi -e "s/www.openstreetmap.org/${OSMSEED_WEB_API_DOMAIN}/g"
+fi
+
 /app/start.sh
