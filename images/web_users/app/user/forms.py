3a9ca5a80f284644340eb71ca487eed5e4ecd8cc
Rub21 <ruben@developmentseed.org>
2022-10-06 09:54:44 -0500

    Update var name

--- images/web_users/app/user/forms.py
+++ images/web_users/app/user/forms.py
@@ -1,7 +1,7 @@
 from .models import Users
 from django import forms
 
-FRUIT_CHOICES = [
+USER_STATUS = [
     ("active", "Active"),
     ("pending", "Pendig"),
     ("confirmed", "Confirmed"),
@@ -14,7 +14,7 @@ class UsersForm(forms.ModelForm):
     email = forms.EmailField(help_text="Enter a valid email address.")
     pass_crypt = forms.CharField(label="Password", required=True)
     display_name = forms.CharField(label="User name", required=True)
-    status = forms.CharField(label="Status", widget=forms.Select(choices=FRUIT_CHOICES))
+    status = forms.CharField(label="Status", widget=forms.Select(choices=USER_STATUS))
 
     class Meta:
         model = Users
