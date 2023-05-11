32f7fe11d6396a3afe24166f1cc0c24ecc558f16
Rub21 <ruben@developmentseed.org>
2023-05-11 14:49:15 -0500

    Fix mapping values - context, job metrics

--- images/osm-simple-metrics/start.sh
+++ images/osm-simple-metrics/start.sh
@@ -5,8 +5,10 @@ CLIS=(cumulative_elements current_elements changeset_counts)
 
 for cli in "${CLIS[@]}"; do
   echo "Star running... $cli"
-  osm-simple-metrics $cli \
-    $conString \
-    -csv \
-    -s3 $AWS_S3_BUCKET
+  if [ $CLOUDPROVIDER == "aws" ]; then
+    osm-simple-metrics $cli \
+      $conString \
+      -csv \
+      -s3 $AWS_S3_BUCKET
+  fi
 done
