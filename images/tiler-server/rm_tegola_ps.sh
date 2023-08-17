c4ce55f4e068b5bfb97ae34f2ef1ed0041ba1482
Rub21 <ruben@developmentseed.org>
2023-08-17 16:58:09 -0500

    Update var names

--- images/tiler-server/rm_tegola_ps.sh
+++ images/tiler-server/rm_tegola_ps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-if [[ -n "${CLEAN_CACHE_MANUALLY}" && "${CLEAN_CACHE_MANUALLY}" == "true" && "${TILER_CACHE_TYPE}" && "${TILER_CACHE_TYPE}" == "s3" ]]; then
+if [[ -n "${KILL_PROCESS}" && "${KILL_PROCESS}" == "manually" ]]; then
     while true; do
-        NUM_PS_TEGOLA=$(ps | grep ${CACHE_PROCESS_NAME} | grep -v grep | wc -l)
-        if [[ $NUM_PS_TEGOLA -gt $MAX_PS_TEGOLA ]]; then
+        NUM_PS=$(ps | grep ${PROCESS_NAME} | grep -v grep | wc -l)
+        if [[ $NUM_PS -gt $MAX_NUM_PS ]]; then
             aws s3 rm s3://${TILER_CACHE_BUCKET}/mnt/data/osm/ --recursive
-            echo "${CACHE_PROCESS_NAME} processes"
-            ps aux | grep ${CACHE_PROCESS_NAME} | grep -v grep
+            echo "${PROCESS_NAME} processes"
+            ps aux | grep ${PROCESS_NAME} | grep -v grep
             # After clearing the S3 cache, terminate all 'tegola' processes.
-            killall ${CACHE_PROCESS_NAME}
+            killall ${PROCESS_NAME}
         fi
         sleep 600
     done
