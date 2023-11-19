5a347127725878e92123adfd6328969123765821
Rub21 <ruben@developmentseed.org>
2023-11-19 22:12:44 +0700

    Set file name

--- images/backup-restore/start.sh
+++ images/backup-restore/start.sh
@@ -63,32 +63,31 @@ restoreDB() {
 		wget -O ${RESTORE_FILE} ${RESTORE_URL_FILE}
 		echo "Restoring ${RESTORE_URL_FILE} in ${POSTGRES_DB}"
 		gunzip -c <${RESTORE_FILE} | psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DB} | tee ${LOG_RESULT_FILE}
-		aws s3 cp ${LOG_RESULT_FILE} s3://${AWS_S3_BUCKET}/
+		aws s3 cp ${LOG_RESULT_FILE} s3://${AWS_S3_BUCKET}/${LOG_RESULT_FILE}
 		echo "Import data to ${POSTGRES_DB} has finished ..."
 	done
 }
 
 delete_old_s3_files() {
-    # Use RETENTION_DAYS from environment variable or default to 30 days
-    if [ -z "${RETENTION_DAYS}" ]; then
-        DAYS_AGO=30
-    else
-        DAYS_AGO="${RETENTION_DAYS}"
-    fi
+	# Use RETENTION_DAYS from environment variable or default to 30 days
+	if [ -z "${RETENTION_DAYS}" ]; then
+		DAYS_AGO=30
+	else
+		DAYS_AGO="${RETENTION_DAYS}"
+	fi
 
-    echo "Files older than $DAYS_AGO days will be deleted."
-    echo "Processing s3://${AWS_S3_BUCKET}/${BACKUP_CLOUD_FOLDER}/"
-    TARGET_DATE=$(date -d "${DAYS_AGO} days ago" +%Y-%m-%d)
-    aws s3 ls "s3://${AWS_S3_BUCKET}/${BACKUP_CLOUD_FOLDER}/" --recursive | while read -r line; do
-        FILE_DATE=$(echo "$line" | awk '{print $1}')
-        FILE_PATH=$(echo "$line" | awk '{print $4}')
-        if [[ "$FILE_DATE" < "$TARGET_DATE" && ! -z "$FILE_PATH" ]]; then
-            echo "Deleting ${FILE_PATH} which was modified on ${FILE_DATE}"
-            aws s3 rm "s3://${AWS_S3_BUCKET}/${FILE_PATH}"
-        fi
-    done
+	echo "Files older than $DAYS_AGO days will be deleted."
+	echo "Processing s3://${AWS_S3_BUCKET}/${BACKUP_CLOUD_FOLDER}/"
+	TARGET_DATE=$(date -d "${DAYS_AGO} days ago" +%Y-%m-%d)
+	aws s3 ls "s3://${AWS_S3_BUCKET}/${BACKUP_CLOUD_FOLDER}/" --recursive | while read -r line; do
+		FILE_DATE=$(echo "$line" | awk '{print $1}')
+		FILE_PATH=$(echo "$line" | awk '{print $4}')
+		if [[ "$FILE_DATE" < "$TARGET_DATE" && ! -z "$FILE_PATH" ]]; then
+			echo "Deleting ${FILE_PATH} which was modified on ${FILE_DATE}"
+			aws s3 rm "s3://${AWS_S3_BUCKET}/${FILE_PATH}"
+		fi
+	done
 }
-
 
 # Main logic
 case "${DB_ACTION}" in
@@ -106,7 +105,7 @@ esac
 
 # Check for the CLEAN_BACKUPS var
 if [ "$CLEANUP_BACKUPS" == "true" ]; then
-    delete_old_s3_files
+	delete_old_s3_files
 else
-    echo "CLEANUP_BACKUPS is not set to true. Skipping deletion."
+	echo "CLEANUP_BACKUPS is not set to true. Skipping deletion."
 fi
