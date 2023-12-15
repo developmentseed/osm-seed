6ef045e01486a88cb7b57e8ca2719f381e02fc05
Rub21 <ruben@developmentseed.org>
2023-12-15 07:23:55 -0500

    Refactoring osmcha
    Update osmcha-deployments
    
    Remove osmcha-api in chartpress

--- images/osmcha-web/start.sh
+++ images/osmcha-web/start.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -ex
+sed -i "s|https://osmcha.org|$OSMCHA_URL|g" package.json
+yarn build:prod
+cp -R build/* /staticfiles/
 
-# Build frontend
-cd /osmcha-frontend
-REACT_APP_VERSION=ohm REACT_APP_STACK=PRODUCTION PUBLIC_URL=$OSMCHA_URL npx react-scripts build
-cp -R build/*.html /app/osmchadjango/frontend/templates/frontend/
-cp -R build/* /app/osmchadjango/static/
-cp -R build/static/* /app/osmchadjango/static/
-
-# Start service
-cd /app
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
-supervisord -c /etc/supervisor/supervisord.conf
+# mkdir -p /staticfiles/static/
+# chmod a+rw /staticfiles/static/
