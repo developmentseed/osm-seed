772fc9126f2e88a56766ebee7534e24b79a72234
Rub21 <ruben@developmentseed.org>
2023-12-19 14:16:59 -0500

    Update osmcha deployment
    Update osmcha deployment
    
    Update services

--- images/osmcha-web/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-sed -i "s|https://osmcha.org|$OSMCHA_URL|g" package.json
-yarn build:prod
-cp -R build/* /staticfiles/
-
-# mkdir -p /staticfiles/static/
-# chmod a+rw /staticfiles/static/
