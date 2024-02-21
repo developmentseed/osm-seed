a0bce980624b2bdeae0d82b55c3e189037c6370a
Rub21 <rub2106@gmail.com>
2024-02-21 18:43:20 -0500

    Update web container

--- images/web/start.sh
+++ images/web/start.sh
@@ -14,7 +14,7 @@ production:
   encoding: utf8" >$workdir/config/database.yml
 
 #### SETTING UP SERVER_URL AND SERVER_PROTOCOL
-sed -i -e 's/server_url: "openstreetmap.example.com"/server_url: "'$SERVER_URL'"/g' $workdir/config/settings.yml
+sed -i -e 's/server_url: "openhistoricalmap.example.com"/server_url: "'$SERVER_URL'"/g' $workdir/config/settings.yml
 sed -i -e 's/server_protocol: "http"/server_protocol: "'$SERVER_PROTOCOL'"/g' $workdir/config/settings.yml
 
 #### SETTING UP MAIL SENDER
@@ -28,14 +28,22 @@ sed -i -e 's/openstreetmap@example.com/'$MAILER_FROM'/g' $workdir/config/setting
 sed -i -e 's/smtp_port: 25/smtp_port: '$MAILER_PORT'/g' $workdir/config/settings.yml
 
 #### SET UP ID KEY
-sed -i -e 's/#id_key: ""/id_key: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
+sed -i -e 's/id_application: ""/id_application: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
 
 ### SET UP OAUTH ID AND KEY
 sed -i -e 's/OAUTH_CLIENT_ID/'$OAUTH_CLIENT_ID'/g' $workdir/config/settings.yml
 sed -i -e 's/OAUTH_KEY/'$OAUTH_KEY'/g' $workdir/config/settings.yml
 
+#### Setup env vars for memcached server
+sed -i -e 's/#memcache_servers: \[\]/memcache_servers: "'$OPENSTREETMAP_memcache_servers'"/g' $workdir/config/settings.yml
+
 ## SET NOMINATIM URL
-sed -i -e 's/nominatim.openstreetmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
+sed -i -e 's/nominatim.openhistoricalmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
+
+## SET OVERPASS URL
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/config/settings.yml
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/views/site/export.html.erb
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/assets/javascripts/index/export.js
 
 #### CHECK IF DB IS ALREADY UP AND START THE APP
 flag=true
@@ -48,8 +56,21 @@ while "$flag" = true; do
     sleep 2
   done &
 
-  # bundle exec rake assets:precompile --trace
+  # Enable assets:precompile, to take lates changes for assets in $workdir/config/settings.yml.
+  time bundle exec rake i18n:js:export assets:precompile
+
   bundle exec rails db:migrate
+
+  # Start lighttpd and cgimap
+  /usr/local/bin/openstreetmap-cgimap \
+    --port=8000 \
+    --daemon \
+    --instances=10 \
+    --dbname=$POSTGRES_DB \
+    --host=$POSTGRES_HOST \
+    --username=$POSTGRES_USER \
+    --password=$POSTGRES_PASSWORD \
+    --logfile log/cgimap.log
   # Start the delayed jobs queue worker and  Start the app
   bundle exec rake jobs:work &
   apachectl -k start -DFOREGROUND
