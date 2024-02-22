49896f19f10d0d1e2de83b04f646615b9ad4c804
Rub21 <rub2106@gmail.com>
2024-02-22 11:34:21 -0500

    Update config to set up web and tm

--- images/web/start.sh
+++ images/web/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 workdir="/var/www"
 export RAILS_ENV=production
-#### Because we can not set up many env variable in build process, we are going to process here!
 
 #### SETTING UP THE PRODUCTION DATABASE
 echo " # Production DB
@@ -14,7 +13,7 @@ production:
   encoding: utf8" >$workdir/config/database.yml
 
 #### SETTING UP SERVER_URL AND SERVER_PROTOCOL
-sed -i -e 's/server_url: "openhistoricalmap.example.com"/server_url: "'$SERVER_URL'"/g' $workdir/config/settings.yml
+sed -i -e 's/server_url: "openstreetmap.example.com"/server_url: "'$SERVER_URL'"/g' $workdir/config/settings.yml
 sed -i -e 's/server_protocol: "http"/server_protocol: "'$SERVER_PROTOCOL'"/g' $workdir/config/settings.yml
 
 #### SETTING UP MAIL SENDER
@@ -27,10 +26,10 @@ sed -i -e 's/smtp_password: null/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir
 sed -i -e 's/openstreetmap@example.com/'$MAILER_FROM'/g' $workdir/config/settings.yml
 sed -i -e 's/smtp_port: 25/smtp_port: '$MAILER_PORT'/g' $workdir/config/settings.yml
 
-#### SET UP ID KEY
+### SET UP ID KEY
 sed -i -e 's/id_application: ""/id_application: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
 
-### SET UP OAUTH ID AND KEY
+## SET UP OAUTH ID AND KEY
 sed -i -e 's/OAUTH_CLIENT_ID/'$OAUTH_CLIENT_ID'/g' $workdir/config/settings.yml
 sed -i -e 's/OAUTH_KEY/'$OAUTH_KEY'/g' $workdir/config/settings.yml
 
@@ -38,40 +37,42 @@ sed -i -e 's/OAUTH_KEY/'$OAUTH_KEY'/g' $workdir/config/settings.yml
 sed -i -e 's/#memcache_servers: \[\]/memcache_servers: "'$OPENSTREETMAP_memcache_servers'"/g' $workdir/config/settings.yml
 
 ## SET NOMINATIM URL
-sed -i -e 's/nominatim.openhistoricalmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
+sed -i -e 's/nominatim.openstreetmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
 
-## SET OVERPASS URL
+# SET OVERPASS URL
 sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/config/settings.yml
 sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/views/site/export.html.erb
 sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/assets/javascripts/index/export.js
 
 #### CHECK IF DB IS ALREADY UP AND START THE APP
 flag=true
+site_loading=true
+
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
   flag=false
-  # Print the log while compiling the assets
+
   until $(curl -sf -o /dev/null $SERVER_URL); do
-    echo "Waiting to start rails ports server..."
+    if [ "$site_loading" = true ]; then
+      echo "Waiting to start Rails ports server..."
+      site_loading=false
+    fi
     sleep 2
   done &
 
-  # Enable assets:precompile, to take lates changes for assets in $workdir/config/settings.yml.
   time bundle exec rake i18n:js:export assets:precompile
 
   bundle exec rails db:migrate
 
-  # Start lighttpd and cgimap
-  /usr/local/bin/openstreetmap-cgimap \
-    --port=8000 \
-    --daemon \
-    --instances=10 \
-    --dbname=$POSTGRES_DB \
-    --host=$POSTGRES_HOST \
-    --username=$POSTGRES_USER \
-    --password=$POSTGRES_PASSWORD \
-    --logfile log/cgimap.log
-  # Start the delayed jobs queue worker and  Start the app
+  # /usr/local/bin/openstreetmap-cgimap \
+  #   --port=8000 \
+  #   --daemon \
+  #   --instances=10 \
+  #   --dbname=$POSTGRES_DB \
+  #   --host=$POSTGRES_HOST \
+  #   --username=$POSTGRES_USER \
+  #   --password=$POSTGRES_PASSWORD \
+  #   --logfile log/cgimap.log
   bundle exec rake jobs:work &
   apachectl -k start -DFOREGROUND
 done
