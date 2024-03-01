0866e43e18cc2bf00ce9e1cbc0fb29427b4a9874
Rub21 <rub2106@gmail.com>
2024-03-01 00:06:03 +0000

    Update private key creation

--- images/web/start.sh
+++ images/web/start.sh
@@ -25,7 +25,7 @@ sed -i -e 's/smtp_user_name: null/smtp_user_name: "'$MAILER_USERNAME'"/g' $workd
 sed -i -e 's/smtp_password: null/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir/config/settings.yml
 sed -i -e 's/openstreetmap@example.com/'$MAILER_FROM'/g' $workdir/config/settings.yml
 sed -i -e 's/smtp_port: 25/smtp_port: '$MAILER_PORT'/g' $workdir/config/settings.yml
-
+ 
 ### SETTING UP UP OAUTH-2 ID KEY FOR iD
 sed -i -e 's/id_application: ""/id_application: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
 
@@ -49,12 +49,11 @@ sed -i -e 's/OpenStreetMap/'$ORGANIZATION_NAME'/g' $workdir/config/settings.yml
 ORGANIZATION_NAME_LOWER=$(echo "$ORGANIZATION_NAME" | tr '[:upper:]' '[:lower:]')
 sed -i -e 's/openstreetmap/'"$ORGANIZATION_NAME_LOWER"'/g' "$workdir/config/settings.yml"
 
-# ADDING DOORKEEPER_SIGNING_KEY
-openssl genpkey -algorithm RSA -out private.pem -aes256
+### ADDING DOORKEEPER_SIGNING_KEY
+openssl genpkey -algorithm RSA -out private.pem
 chmod 400 /var/www/private.pem
 export DOORKEEPER_SIGNING_KEY=$(cat /var/www/private.pem | sed -e '1d;$d' | tr -d '\n')
 sed -i "s#PRIVATE_KEY#${DOORKEEPER_SIGNING_KEY}#" $workdir/config/settings.yml
-
 
 #### CHECK IF DB IS ALREADY UP AND START THE APP
 flag=true
@@ -81,7 +80,7 @@ while "$flag" = true; do
   #   --host=$POSTGRES_HOST \
   #   --username=$POSTGRES_USER \
   #   --password=$POSTGRES_PASSWORD \
-  # --logfile log/cgimap.log
+  #   --logfile log/cgimap.log
   bundle exec rake jobs:work &
   apachectl -k start -DFOREGROUND
 done
