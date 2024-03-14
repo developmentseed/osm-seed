5f1dfdd63f2d15c0a920b30a47e04f450e009705
Ruben L. Mendoza <rub2106@gmail.com>
2024-03-14 15:53:39 -0500

    Update web container and TM config (#315)
    * Update config for TM to use a db
    
    * Update web container
    
    * Update config to set up web and tm
    
    * Update env vars for web and tm
    
    * Restart containers
    
    * Update port for planet api
    
    * Update tiler containers
    
    * Update config
    
    * Update compose config
    
    * Delete tasking manager web
    
    * Update website version
    
    * Update config
    
    * Generate doorkeeper_signing_key and set in config
    
    * Remove upload to s3 the logs from restore db
    
    * Add env var to change org name
    
    * Disable cgimap
    
    * Update the private key creation
    
    * Comment cgimap cli
    
    * Update private key creation
    
    * Update website version
    
    * Update api file service
    
    * Update api db

--- images/web/start.sh
+++ images/web/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 workdir="/var/www"
 export RAILS_ENV=production
-#### Because we can not set up many env variable in build process, we are going to process here!
 
 #### SETTING UP THE PRODUCTION DATABASE
 echo " # Production DB
@@ -26,31 +25,62 @@ sed -i -e 's/smtp_user_name: null/smtp_user_name: "'$MAILER_USERNAME'"/g' $workd
 sed -i -e 's/smtp_password: null/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir/config/settings.yml
 sed -i -e 's/openstreetmap@example.com/'$MAILER_FROM'/g' $workdir/config/settings.yml
 sed -i -e 's/smtp_port: 25/smtp_port: '$MAILER_PORT'/g' $workdir/config/settings.yml
+ 
+### SETTING UP UP OAUTH-2 ID KEY FOR iD
+sed -i -e 's/id_application: ""/id_application: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
 
-#### SET UP ID KEY
-sed -i -e 's/#id_key: ""/id_key: "'$OPENSTREETMAP_id_key'"/g' $workdir/config/settings.yml
-
-### SET UP OAUTH ID AND KEY
+### SETTING UP OAUTH-2 ID KEY WEBSITE
 sed -i -e 's/OAUTH_CLIENT_ID/'$OAUTH_CLIENT_ID'/g' $workdir/config/settings.yml
 sed -i -e 's/OAUTH_KEY/'$OAUTH_KEY'/g' $workdir/config/settings.yml
 
-## SET NOMINATIM URL
+#### SETTING UP ENV VARS FOR MEMCACHED SERVER
+sed -i -e 's/#memcache_servers: \[\]/memcache_servers: "'$OPENSTREETMAP_memcache_servers'"/g' $workdir/config/settings.yml
+
+### SETTING UP NOMINATIM URL
 sed -i -e 's/nominatim.openstreetmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
+
+#### SETTING UP OVERPASS URL
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/config/settings.yml
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/views/site/export.html.erb
+sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/assets/javascripts/index/export.js
+
+### SETTING UP ORGANIZATION
+sed -i -e 's/OpenStreetMap/'$ORGANIZATION_NAME'/g' $workdir/config/settings.yml
+ORGANIZATION_NAME_LOWER=$(echo "$ORGANIZATION_NAME" | tr '[:upper:]' '[:lower:]')
+sed -i -e 's/openstreetmap/'"$ORGANIZATION_NAME_LOWER"'/g' "$workdir/config/settings.yml"
+
+### ADDING DOORKEEPER_SIGNING_KEY
+openssl genpkey -algorithm RSA -out private.pem
+chmod 400 /var/www/private.pem
+export DOORKEEPER_SIGNING_KEY=$(cat /var/www/private.pem | sed -e '1d;$d' | tr -d '\n')
+sed -i "s#PRIVATE_KEY#${DOORKEEPER_SIGNING_KEY}#" $workdir/config/settings.yml
 
 #### CHECK IF DB IS ALREADY UP AND START THE APP
 flag=true
+site_loading=true
+
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -p 5432 >/dev/null 2>&2 || continue
   flag=false
-  # Print the log while compiling the assets
+
   until $(curl -sf -o /dev/null $SERVER_URL); do
-    echo "Waiting to start rails ports server..."
+    if [ "$site_loading" = true ]; then
+      echo "Waiting to start Rails ports server..."
+      site_loading=false
+    fi
     sleep 2
   done &
-
-  # bundle exec rake assets:precompile --trace
+  # time rails i18n:js:export assets:precompile
   bundle exec rails db:migrate
-  # Start the delayed jobs queue worker and  Start the app
+  # /usr/local/bin/openstreetmap-cgimap \
+  #   --port=8000 \
+  #   --daemon \
+  #   --instances=10 \
+  #   --dbname=$POSTGRES_DB \
+  #   --host=$POSTGRES_HOST \
+  #   --username=$POSTGRES_USER \
+  #   --password=$POSTGRES_PASSWORD \
+  #   --logfile log/cgimap.log
   bundle exec rake jobs:work &
   apachectl -k start -DFOREGROUND
 done
