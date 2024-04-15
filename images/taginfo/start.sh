bf923f448dd34c973dc905bd253fab21d9effa71
Rub21 <rub2106@gmail.com>
2024-04-15 18:33:29 -0500

    Fix name of the env var for AWS_S3_BUCKET

--- images/taginfo/start.sh
+++ images/taginfo/start.sh
@@ -61,8 +61,8 @@ compress_files() {
 }
 
 download_db_files() {
-    if ! aws s3 ls "s3://$BUCKET_NAME/$ENVIRONMENT" 2>&1 | grep -q 'An error occurred'; then
-        aws s3 sync "s3://$BUCKET_NAME/$ENVIRONMENT/" "$DATADIR/"
+    if ! aws s3 ls "s3://$AWS_S3_BUCKET/$ENVIRONMENT" 2>&1 | grep -q 'An error occurred'; then
+        aws s3 sync "s3://$AWS_S3_BUCKET/$ENVIRONMENT/" "$DATADIR/"
     fi
     compress_files
 }
