9a90c8a188ac1e4371272103441127f9d39a9971
Rub21 <rub2106@gmail.com>
2024-06-20 13:17:43 -0500

    Update cgimap dockerfile

--- images/cgimap/start.sh
+++ images/cgimap/start.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
-set -ex
+export CGIMAP_HOST=$POSTGRES_HOST
+export CGIMAP_DBNAME=$POSTGRES_DB
+export CGIMAP_USERNAME=$POSTGRES_USER
+export CGIMAP_PASSWORD=$POSTGRES_PASSWORD
+export CGIMAP_OAUTH_HOST=$POSTGRES_HOST
+export CGIMAP_UPDATE_HOST=$POSTGRES_HOST
+export CGIMAP_LOGFILE="/var/www/log/cgimap.log"
+export CGIMAP_MEMCACHE=$OPENSTREETMAP_memcache_servers
+export CGIMAP_RATELIMIT="204800"
+export CGIMAP_MAXDEBT="250"
+export CGIMAP_MAP_AREA="0.25"
+export CGIMAP_MAP_NODES="100000"
+export CGIMAP_MAX_WAY_NODES="2000"
+export CGIMAP_MAX_RELATION_MEMBERS="32000"
+# export CGIMAP_RATELIMIT_UPLOAD="true"
+# export CGIMAP_MODERATOR_RATELIMIT="1048576"
+# export CGIMAP_MODERATOR_MAXDEBT="1280"
+# export CGIMAP_PIDFILE="/var/www/cgimap.pid"
 
-# Make sure that the follow env vars has been declare
-# API_WEB_HOST
-# API_WEB_PORT
-envsubst < lighttpd.conf.template > lighttpd.conf
-/usr/sbin/lighttpd -f lighttpd.conf
-
-/usr/local/bin/openstreetmap-cgimap \
-	--port=8000 \
-	--instances=30 \
-	--dbname=$POSTGRES_DB \
-	--host=$POSTGRES_HOST \
-	--username=$POSTGRES_USER \
-	--password=$POSTGRES_PASSWORD
-
+# Check for web site status
+if [[ "$WEBSITE_STATUS" == "database_readonly" || "$WEBSITE_STATUS" == "api_readonly" ]]; then
+  export CGIMAP_DISABLE_API_WRITE="true"
+fi
+if [[ "$WEBSITE_STATUS" == "database_offline" || "$WEBSITE_STATUS" == "api_offline" ]]; then
+    echo "Website is $WEBSITE_STATUS. No action required for cgimap service."
+else
+  /usr/local/bin/openstreetmap-cgimap --port=8000 --daemon --instances=10 && \
+    tail --pid=$(cat /tmp/cgimap.pid) -f /dev/null
+fi
