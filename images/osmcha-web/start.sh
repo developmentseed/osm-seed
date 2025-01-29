6a3c690784139654c33c9af45ba7b8d432f78929
Rub21 <rub2106@gmail.com>
2025-01-29 16:06:50 -0500

    Update livenessProbe path

--- images/osmcha-web/start.sh
+++ images/osmcha-web/start.sh
@@ -2,7 +2,4 @@
 set -x
 export BUILD_ENV=prod
 export REACT_APP_PRODUCTION_API_URL=/api/v1
-sed -i "s|https://osmcha.org|$OSMCHA_URL|g" package.json
-yarn build:${BUILD_ENV}
-find /app/build -type f -exec sed -i "s/www.openstreetmap.org/$OSMCHA_API_URL/g" {} +
-cp -r /app/build/* /assets/
+yarn build
