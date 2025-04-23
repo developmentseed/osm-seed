49d677b34886e857faf6a75e6862af1b20e982d4
Ruben L. Mendoza <rub2106@gmail.com>
2025-04-22 21:53:46 -0500

    CGIMAP container (#300)
    * Add cgmap container
    
    * Correct the misspelled name
    
    * Add helm template for cgimap
    
    * Update cgimap dockerfile
    
    * Update tiler-imposm dockerfile
    
    * Update python
    
    * Update cgimap container
    
    * Update cgimap container
    
    * Update container for cgimap
    
    * Build cgimap image using multistage
    
    * Update cgimap config
    
    Update relese tag
    
    Pass env vars in web container for cgimap
    
    Update release tag
    
    Update tag name
    
    Update release tags
    
    * Update release tags
    
    * Add memcache server for cgimap
    
    * Update cgimap start and github action
    
    * Add --daemon for cgimap
    
    * Set empty values for planetFiles.image
    
    * Disable cgimap  in chartpress
    
    * Use chartpress==2.3.0
    
    * Add HorizontalPodAutoscaler for cgimap
    
    * Update web-api container
    
    * Fix apache config
    
    * Add PG password for tiler-db config as env var
    
    * Update cgimap dockerfile using code form original repo
    
    * Enable resources option for requests and limits - cgimap
    
    * Evaluate memoryUtilization for web container - autoscaling
    
    ---------
    
    Co-authored-by: Rub21 <ruben@developmentseed.org>

--- images/web/liveness.sh
+++ images/web/liveness.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This is a script for evaluating if openstreetmap-cgimap, apache2, and PostgreSQL are running in the container.
+# This is a script for evaluating if apache2 is running in the container and PostgreSQL is reachable. 
 check_process() {
     if ps aux | grep "$1" | grep -v grep > /dev/null; then
         return 0
@@ -8,29 +8,24 @@ check_process() {
     fi
 }
 
-# Check for openstreetmap-cgimap process
-check_process "/openstreetmap-cgimap/build/openstreetmap-cgimap"
-cgimap_status=$?
-
 # Check for apache2 process
 check_process "apache2"
 apache_status=$?
 
 # Check PostgreSQL connection
 check_postgres() {
-    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 1;" > /dev/null 2>&1
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT 1;" > /dev/null 2>&1
     return $?
 }
 
 check_postgres
 postgres_status=$?
 
-if [ $cgimap_status -eq 0 ] && [ $apache_status -eq 0 ] && [ $postgres_status -eq 0 ]; then
-    echo "All services (openstreetmap-cgimap, apache2, PostgreSQL) are running."
+if [ $apache_status -eq 0 ] && [ $postgres_status -eq 0 ]; then
+    echo "Apache and PostgreSQL are running."
     exit 0
 else
-    [ $cgimap_status -ne 0 ] && echo "openstreetmap-cgimap is not running!" 1>&2
-    [ $apache_status -ne 0 ] && echo "apache2 is not running!" 1>&2
-    [ $postgres_status -ne 0 ] && echo "Failed to connect to PostgreSQL!" 1>&2
+    [ $apache_status -ne 0 ] && echo "apache2 is not running!" >&2
+    [ $postgres_status -ne 0 ] && echo "Failed to connect to PostgreSQL!" >&2
     exit 1
 fi
