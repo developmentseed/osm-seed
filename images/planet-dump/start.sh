ee57c16be0cae03522fb30f998ab9714c84d85b5
Rub21 <rub2106@gmail.com>
2025-06-30 15:04:48 -0500

    Update scripts and continaer for planet-dump-ng

--- images/planet-dump/start.sh
+++ images/planet-dump/start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-export VOLUME_DIR=/mnt/data
 
 # osmosis tuning: https://wiki.openstreetmap.org/wiki/Osmosis/Tuning,https://lists.openstreetmap.org/pipermail/talk/2012-October/064771.html
 if [ -z "$MEMORY_JAVACMD_OPTIONS" ]; then
@@ -10,64 +9,83 @@ else
 	echo JAVACMD_OPTIONS=\"-server -Xmx$memory\" >~/.osmosis
 fi
 
-# Read the DB and create the planet osm file
+export VOLUME_DIR=/mnt/data
 date=$(date '+%y%m%d_%H%M')
+
 local_planetPBFFile=$VOLUME_DIR/planet-${date}.osm.pbf
 cloud_planetPBFFile=planet/planet-${date}.osm.pbf
+stateFile="$VOLUME_DIR/state.txt"
 
-# In case overwrite the file
+# If overwrite flag is enabled, use fixed filenames
 if [ "$OVERWRITE_PLANET_FILE" == "true" ]; then
 	local_planetPBFFile=$VOLUME_DIR/planet-latest.osm.pbf
 	cloud_planetPBFFile=planet/planet-latest.osm.pbf
 fi
 
-stateFile="$VOLUME_DIR/state.txt"
+# ===============================
+# Download db .dump file 
+# ===============================
+download_dump_file() {
+	local_dumpFile="$VOLUME_DIR/input-latest.dump"
+	echo "Downloading db .dump file from cloud..."
 
-# Creating the replication file
-osmosis --read-apidb \
-	host=$POSTGRES_HOST \
-	database=$POSTGRES_DB \
-	user=$POSTGRES_USER \
-	password=$POSTGRES_PASSWORD \
-	validateSchemaVersion=no \
-	--write-pbf \
-	file=$local_planetPBFFile
+	if [ "$CLOUDPROVIDER" == "aws" ]; then
+		aws s3 cp "$DUMP_CLOUD_URL" "$local_dumpFile"
+	elif [ "$CLOUDPROVIDER" == "gcp" ]; then
+		gsutil cp "$DUMP_CLOUD_URL" "$local_dumpFile"
+	fi
+}
 
-# AWS
-if [ $CLOUDPROVIDER == "aws" ]; then
-	# Save the path file
-	AWS_URL=${AWS_S3_BUCKET/s3:\/\//http:\/\/}
-	echo "$AWS_URL.s3.amazonaws.com/$cloud_planetPBFFile" > $stateFile
-	# Upload planet.osm.pbf file to s3
-	aws s3 cp $local_planetPBFFile $AWS_S3_BUCKET/$cloud_planetPBFFile --acl public-read
-	# Upload state.txt file to s3
-	aws s3 cp $stateFile $AWS_S3_BUCKET/planet/state.txt --acl public-read
+# ===============================
+# Upload planet + state
+# ===============================
+upload_planet_file() {
+	echo "Uploading planet file and updating state.txt..."
+
+	if [ "$CLOUDPROVIDER" == "aws" ]; then
+		AWS_URL=${AWS_S3_BUCKET/s3:\/\//http:\/\/}
+		echo "$AWS_URL.s3.amazonaws.com/$cloud_planetPBFFile" > "$stateFile"
+		aws s3 cp "$local_planetPBFFile" "$AWS_S3_BUCKET/$cloud_planetPBFFile" --acl public-read
+		aws s3 cp "$stateFile" "$AWS_S3_BUCKET/planet/state.txt" --acl public-read
+
+	elif [ "$CLOUDPROVIDER" == "gcp" ]; then
+		echo "https://storage.cloud.google.com/$GCP_STORAGE_BUCKET/$cloud_planetPBFFile" > "$stateFile"
+		gsutil cp -a public-read "$local_planetPBFFile" "$GCP_STORAGE_BUCKET/$cloud_planetPBFFile"
+		gsutil cp -a public-read "$stateFile" "$GCP_STORAGE_BUCKET/planet/state.txt"
+	fi
+}
+
+# ===============================
+# Generate planet file
+# ===============================
+
+if [ "$PLANET_EXPORT_METHOD" == "planet-dump-ng" ]; then
+	download_dump_file
+	echo "Generating planet file with planet-dump-ng..."
+	planet-dump-ng \
+		--dump-file "$VOLUME_DIR/input-latest.dump" \
+		--pbf "$local_planetPBFFile"
+elif [ "$PLANET_EXPORT_METHOD" == "osmosis" ]; then
+	echo "Generating planet file with osmosis..."
+	if [ -z "$MEMORY_JAVACMD_OPTIONS" ]; then
+		echo JAVACMD_OPTIONS=\"-server\" > ~/.osmosis
+	else
+		memory="${MEMORY_JAVACMD_OPTIONS//i/}"
+		echo JAVACMD_OPTIONS=\"-server -Xmx$memory\" > ~/.osmosis
+	fi
+
+	osmosis --read-apidb \
+		host=$POSTGRES_HOST \
+		database=$POSTGRES_DB \
+		user=$POSTGRES_USER \
+		password=$POSTGRES_PASSWORD \
+		validateSchemaVersion=no \
+		--write-pbf \
+		file=$local_planetPBFFile
+else
+	echo "Error: Unknown PLANET_EXPORT_METHOD value. Use 'planet-dump-ng' or 'osmosis'."
+	exit 1
 fi
 
-# gcp
-if [ $CLOUDPROVIDER == "gcp" ]; then
-	# Save the path file
-	echo "https://storage.cloud.google.com/$GCP_STORAGE_BUCKET/$cloud_planetPBFFile" > $stateFile
-	# Upload planet.osm.pbf file to cloud storage
-	gsutil cp -a public-read $local_planetPBFFile $GCP_STORAGE_BUCKET/$cloud_planetPBFFile
-	# Upload state.txt file to cloud storage
-	gsutil cp -a public-read $stateFile $GCP_STORAGE_BUCKET/planet/state.txt
-fi
-
-# Azure
-if [ $CLOUDPROVIDER == "azure" ]; then
-	# Save the path file
-	echo "https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/$AZURE_CONTAINER_NAME/$cloud_planetPBFFile" > $stateFile
-	# Upload planet.osm.pbf file to blob storage
-	az storage blob upload \
-        --container-name $AZURE_CONTAINER_NAME \
-        --file $local_planetPBFFile \
-        --name $cloud_planetPBFFile \
-        --output table
-	# Upload state.txt file to blob storage
-	az storage blob upload \
-        --container-name $AZURE_CONTAINER_NAME \
-        --file $stateFile \
-        --name planet/state.txt \
-        --output table
-fi
+# Upload results
+upload_planet_file
