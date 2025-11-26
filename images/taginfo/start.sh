e58293f73a4622a5eb57981e36c6974da11f4125
Rub21 <rub2106@gmail.com>
2025-11-26 12:49:49 -0500

    Add ulr path to download files
    Update taginfo scrpit
    
    Fix scrpit
    
    Fix config for taginfo serviceAccount

--- images/taginfo/start.sh
+++ images/taginfo/start.sh
@@ -46,12 +46,8 @@ process_data() {
     # wikidata/update.sh $DATADIR
     chronology/update.sh $DATADIR
     ./update_all.sh $DATADIR
-    mv $DATADIR/*.db $DATADIR/
-    mv $DATADIR/*/*.db $DATADIR/
-    # if AWS_S3_BUCKET is set upload data
-    if ! aws s3 ls "s3://$AWS_S3_BUCKET/taginfo/$ENVIRONMENT" 2>&1 | grep -q 'An error occurred'; then
-        aws s3 sync $DATADIR/ s3://$AWS_S3_BUCKET/taginfo/$ENVIRONMENT/  --exclude "*" --include "*.db"
-    fi
+    # Move database files from subdirectories to main data directory
+    aws s3 sync $DATADIR/ s3://$AWS_S3_BUCKET/taginfo/$ENVIRONMENT/  --exclude "*" --include "*.db"
 }
 
 # Compress files to download
@@ -63,24 +59,60 @@ compress_files() {
 }
 
 download_db_files() {
-    if ! aws s3 ls "s3://$AWS_S3_BUCKET/$ENVIRONMENT" 2>&1 | grep -q 'An error occurred'; then
-        aws s3 sync "s3://$AWS_S3_BUCKET/$ENVIRONMENT/" "$DATADIR/"
-        mv $DATADIR/*.db $DATADIR/
-        mv $DATADIR/*/*.db $DATADIR/
-        compress_files
+    local base_url=$1
+    
+    if [ -z "$base_url" ]; then
+        echo "Error: URL base is required for download_db_files"
+        return 1
     fi
+    
+    # Ensure base_url ends with /
+    if [[ ! "$base_url" =~ /$ ]]; then
+        base_url="${base_url}/"
+    fi
+    
+    # List of SQLite database files to download
+    local db_files=(
+        "projects-cache.db"
+        "selection.db"
+        "taginfo-chronology.db"
+        "taginfo-db.db"
+        "taginfo-history.db"
+        "taginfo-languages.db"
+        "taginfo-master.db"
+        "taginfo-projects.db"
+        "taginfo-wiki.db"
+        "taginfo-wikidata.db"
+    )
+    
+    echo "Downloading SQLite database files from: $base_url"
+    
+    for db_file in "${db_files[@]}"; do
+        local file_url="${base_url}${db_file}"
+        local output_path="${DATADIR}/${db_file}"
+        
+        echo "Downloading: $db_file"
+        if wget -q --show-progress -O "$output_path" --no-check-certificate "$file_url"; then
+            echo "Successfully downloaded: $db_file"
+        else
+            echo "Warning: Failed to download $db_file from $file_url"
+            # Continue with other files even if one fails
+        fi
+    done
+    
+    echo "Database files download completed"
 }
 
 sync_latest_db_version() {
     while true; do
         sleep "$INTERVAL_DOWNLOAD_DATA"
-        download_db_files
+        download_db_files "$TAGINFO_DB_BASE_URL"
     done
 }
 
 start_web() {
     echo "Start...Taginfo web service"
-    download_db_files
+    download_db_files "$TAGINFO_DB_BASE_URL"
     cd $WORKDIR/taginfo/web && ./taginfo.rb & sync_latest_db_version
 }
 
