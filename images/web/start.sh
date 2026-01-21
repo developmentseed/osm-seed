ccff0f0100d63eda9b7a079e7c4aff2167886c3d
noulf <254490363+noulf@users.noreply.github.com>
2026-01-21 17:00:59 +0100

    Simplified setup on first start
    - admin account created
    - default OAuth 2 applications registered
    - allow localhost as server name
    - additional comments in .env.web.example
    - storage can be changed in env file
    - email to smtp relay without password is now possible

--- images/web/start.sh
+++ images/web/start.sh
@@ -42,6 +42,10 @@ EOF
   SERVER_URL_CLEAN=$(echo "$SERVER_URL" | sed 's|/$||')
   sed -i -e 's/^server_protocol: ".*"/server_protocol: "'$SERVER_PROTOCOL'"/g' $workdir/config/settings.yml
   sed -i -e 's/^server_url: ".*"/server_url: "'$SERVER_URL_CLEAN'"/g' $workdir/config/settings.yml
+  if [ "$SERVER_PROTOCOL" == "http" ]; then
+    sed -i -e 's/config.force_ssl = true/config.force_ssl = false/' $workdir/config/environments/production.rb
+    sed -i -e 's/config.assume_ssl = true/config.assume_ssl = false/' $workdir/config/environments/production.rb
+  fi
 
   #### Extract domain from SERVER_URL and replace in production.conf
   SERVER_DOMAIN=$(echo "$SERVER_URL_CLEAN" | sed -e 's|^[^/]*//||' -e 's|^www\.||' -e 's|/.*$||')
@@ -54,9 +58,16 @@ EOF
   sed -i -e 's/smtp_address: ".*"/smtp_address: "'$MAILER_ADDRESS'"/g' $workdir/config/settings.yml
   sed -i -e 's/smtp_port: .*/smtp_port: '$MAILER_PORT'/g' $workdir/config/settings.yml
   sed -i -e 's/smtp_domain: ".*"/smtp_domain: "'$MAILER_DOMAIN'"/g' $workdir/config/settings.yml
-  sed -i -e 's/smtp_authentication: .*/smtp_authentication: "login"/g' $workdir/config/settings.yml
-  sed -i -e 's/smtp_user_name: .*/smtp_user_name: "'$MAILER_USERNAME'"/g' $workdir/config/settings.yml
-  sed -i -e 's/smtp_password: .*/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir/config/settings.yml
+
+  if [ "$MAILER_AUTHENTICATION" == "" ]; then
+    sed -i -e 's/smtp_authentication: .*/smtp_authentication: null/g' $workdir/config/settings.yml
+    sed -i -e 's/smtp_user_name: .*/smtp_user_name: null/g' $workdir/config/settings.yml
+    sed -i -e 's/smtp_password: .*/smtp_password: null/g' $workdir/config/settings.yml
+  else
+    sed -i -e 's/smtp_authentication: .*/smtp_authentication: "'$MAILER_AUTHENTICATION'"/g' $workdir/config/settings.yml
+    sed -i -e 's/smtp_user_name: .*/smtp_user_name: "'$MAILER_USERNAME'"/g' $workdir/config/settings.yml
+    sed -i -e 's/smtp_password: .*/smtp_password: "'$MAILER_PASSWORD'"/g' $workdir/config/settings.yml
+  fi
 
   ### Setting up oauth id and key for iD editor
   sed -i -e 's/^oauth_application: ".*"/oauth_application: "'$OAUTH_CLIENT_ID'"/g' $workdir/config/settings.yml
@@ -68,6 +79,12 @@ EOF
   #### Setup env vars for memcached server
   sed -i -e 's/memcache_servers: \[\]/memcache_servers: "'$OPENSTREETMAP_memcache_servers'"/g' $workdir/config/settings.yml
 
+  ## Setting up storage
+  sed -i -e 's/avatar_storage: ".*"/avatar_storage: "'$OPENSTREETMAP_avatar_storage'"/g' $workdir/config/settings.yml
+  sed -i -e 's/trace_file_storage: ".*"/trace_file_storage: "'$OPENSTREETMAP_trace_file_storage'"/g' $workdir/config/settings.yml
+  sed -i -e 's/trace_image_storage: ".*"/trace_image_storage: "'$OPENSTREETMAP_trace_image_storage'"/g' $workdir/config/settings.yml
+  sed -i -e 's/trace_icon_storage: ".*"/trace_icon_storage: "'$OPENSTREETMAP_trace_icon_storage'"/g' $workdir/config/settings.yml
+
   #### Setting up nominatim url
   sed -i -e 's/nominatim-api.openstreetmap.org/'$NOMINATIM_URL'/g' $workdir/config/settings.yml
 
@@ -77,8 +94,10 @@ EOF
   sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/assets/javascripts/index/export.js
 
   ## Setting up required credentials 
-  echo $RAILS_CREDENTIALS_YML_ENC > config/credentials.yml.enc
-  echo $RAILS_MASTER_KEY > config/master.key 
+  if [ "$RAILS_MASTER_KEY" != "" ]; then
+    echo $RAILS_CREDENTIALS_YML_ENC > config/credentials.yml.enc
+    echo $RAILS_MASTER_KEY > config/master.key
+  fi
   chmod 600 config/credentials.yml.enc config/master.key
 
   #### Adding doorkeeper_signing_key
@@ -86,7 +105,45 @@ EOF
   chmod 400 /var/www/private.pem
   export DOORKEEPER_SIGNING_KEY=$(cat /var/www/private.pem | sed -e '1d;$d' | tr -d '\n')
   sed -i "s#PRIVATE_KEY#${DOORKEEPER_SIGNING_KEY}#" $workdir/config/settings.yml
+}
 
+setup_admin() {
+  ### Create default admin and register default oauth apps
+  if [ $ADMIN_MAIL != "" ] && [ ! -f $workdir/.admin_created ]; then
+    echo "Creating user 'admin' ..."
+    touch $workdir/.admin_created
+
+    bundle exec rails runner "u = User.find_by(:display_name => 'admin'); \
+        if (nil != u) then puts('User already exists'); exit(2) end;
+        u = User.new(email:'$ADMIN_MAIL', display_name: 'admin'); \
+        u.pass_crypt='$ADMIN_PASSWORD'; \
+        u.pass_crypt_confirmation='$ADMIN_PASSWORD'; \
+        u.save"'!'"; \
+        u.activate"'!'"; \
+        u.roles.create(:role => \"moderator\", :granter_id => u.id); \
+        u.roles.create(:role => \"administrator\", :granter_id => u.id); \
+        exit(0);"
+
+    if [ $? -eq 0 ]; then # Success: 0
+      echo "Creating user 'admin': Success!"
+    else # Error: 1
+      echo "Creating user 'admin': Failure!"
+    fi
+
+    bundle exec rails runner 'exit(nil == Oauth2Application.find_by(:name => "Local iD"))' # true converted to result code 0
+    if [ $? -eq 0 ]; then
+      echo "Registering default OAuth2 applications for user 'admin' ..."
+
+      sed -i -e 's/^id_application:/#id_application:/' $workdir/config/settings.yml
+      sed -i -e 's/^oauth_application:/#oauth_application:/' $workdir/config/settings.yml
+      sed -i -e 's/^oauth_key:/#oauth_key:/' $workdir/config/settings.yml
+
+      bundle exec rails oauth:register_apps["admin"]
+    else
+      echo "OAuth2 default applications already exist. No new applications registered."
+    fi
+
+  fi
 }
 
 restore_db() {
@@ -135,6 +192,8 @@ setup_production() {
   echo "Running database migrations..."
   time bundle exec rails db:migrate
 
+  setup_admin
+
   if [ "$EXTERNAL_CGIMAP" == "false" ]; then
     echo "Running cgimap..."
     ./cgimap.sh
@@ -158,6 +217,7 @@ setup_development() {
   setup_env_vars
   bundle exec bin/yarn install
   bundle exec rails db:migrate --trace
+  setup_admin
   bundle exec rake jobs:work &
   rails server --log-to-stdout
 }
